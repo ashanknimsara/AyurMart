8a60e718179a6cb6b21946593602f7b2ce04bd12
Ashan K Nimsara <ashanknimsara@gmail.com>
2023-05-10 17:52:01 +0530

    products routes changes

--- microservices/ProductManagement/routes/ProductRoutes.js
+++ microservices/ProductManagement/routes/ProductRoutes.js
@@ -16,14 +16,14 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-// Localhost:5000/products/new - POST - Create a new product
+// Localhost:3005/products/new - POST - Create a new product
 //router.post("/new", AdminAuth, upload.single("image"), async (req, res) => {
     router.post("/new", upload.single("image"), async (req, res) => {
         try {
-            const { productId, productName, productCategory, productPrice, productImage, productQuantity } = req.body;
-            const image = req.file ? req.file.path : "";
+            const { productId, productName, productCategory, productPrice  } = req.body;
+            const productImage = req.file ? req.file.path : "";
     
-            const product = new Product({ productId, productName, productCategory, productPrice, productImage, productQuantity, image });
+            const product = new Product({ productId, productName, productCategory, productPrice, productImage });
             await product.save();
     
             return res.status(201).json({
@@ -39,3 +39,22 @@ const upload = multer({ storage });
             });
         }
     });
+
+// Localhost:3005/products/all - GET - Get all products
+router.get("/all", async (req, res) => {
+    try {
+        const products = await Product.find();
+        return res.status(200).json({
+            status: true,
+            message: "Products retrieved successfully",
+            data: products,
+        });
+    } catch (error) {
+        return res.status(500).json({
+            status: false,
+            message: "Server error",
+            data: undefined,
+        });
+    }
+});
+module.exports = router;
