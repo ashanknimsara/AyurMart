dd3d0a3be01055547597ae2074fa0a516d863bb5
Ashan K Nimsara <ashanknimsara@gmail.com>
2023-05-12 21:14:28 +0530

    view product and add product frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@
 
 import Home from "./components/LandingPage";
 import ProductPage from "./components/ProductPage"
+import ViewProduct from "./components/ViewProduct";
+import AddProduct from "./components/seller/SellerAddProduct";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -13,6 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/shop" element={<ProductPage />}/>
+          <Route path="/products/:id" element={<ViewProduct />}/>
+          <Route path="/seller/add-product" element={<AddProduct />}/>
         </Routes>
       </BrowserRouter>
     </div>
