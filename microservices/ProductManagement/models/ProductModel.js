a105d49da2103cb0edc55d8562d145f49f3bd72e
Ashan K Nimsara <ashanknimsara@gmail.com>
2023-05-12 15:36:43 +0530

    product page frontend

--- microservices/ProductManagement/models/ProductModel.js
+++ microservices/ProductManagement/models/ProductModel.js
@@ -3,10 +3,11 @@ const mongoose = require("mongoose");
 const productSchema = new mongoose.Schema({
     productId: { type: String, required: true },
     productName: { type: String, required: true },
-    productCategory: { type: String, required: true },
+    productDescription: { type: String, required: true },
     productPrice: { type: String, required: true },
     productQuantity: { type: Number, required: true },
     productImage: { type: String, required: true },
+    sellerId: { type: String, required: true },
     createdAt: { type: Date, default: Date.now },
 });
 
